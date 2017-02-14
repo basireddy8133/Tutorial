ca725e6f5cbc9778e0df2d708edbd6c961cdf8c7
reddy <basavaiah.kallam>
2017-02-14 22:18:07 +0530

    reddy commiting

---
+++ demo/src/main/java/com/example/DemoApplication3.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication3 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication3.class, args);
+	}
+}
+//22222
+//333
+//444
+//copying from somewere
