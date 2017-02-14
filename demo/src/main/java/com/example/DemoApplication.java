c433f053ae163fa86bcb22157a188b6f4a9b2b6e
reddy <basavaiah.kallam>
2017-02-14 22:23:44 +0530

    klrdhfth

---
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
+//111111
+
+//jjjh
+//trhdty
+//hgjhjh
+
