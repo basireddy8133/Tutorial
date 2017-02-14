62dca7381f9745ce12bbe4f2c986e4861e2a2af5
reddy <basavaiah.kallam>
2017-02-14 22:42:41 +0530

    hello

---
+++ demo/src/main/java/com/example/DemoApplication3.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication3 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication3.class, args);
+	}
+}
+//111111
+
+//pppp
